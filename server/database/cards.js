68105fe905f8d87a7026caafe4bd3d4ae3222dd8
Mikhail <miffn3@yandex.ru>
2019-10-24 21:43:00 +0400

    back-end-init

---
+++ server/database/cards.js
@@ -0,0 +1,20 @@
+export const cards = [
+  {
+    id: 1,
+    name: "Reebok Jacket",
+    price: "199$",
+    colors: {},
+  },
+  {
+    id: 2,
+    name: "Reebok Jacket",
+    price: "199$",
+    colors: {},
+  },
+  {
+    id: 3,
+    name: "Reebok Jacket",
+    price: "199$",
+    colors: {},
+  }
+]
