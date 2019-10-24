c26c282fb1826111ccd9be45927326a2895e8841
EasileleM <pro100prosavva@gmail.com>
2019-10-24 20:07:47 +0400

    a

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,9 @@ frontend_target="FRONTEND"
 if [ "$APP_SIDE" = "$frontend_target" ]; then
   cd client
   npm install
-  npm run start
+  npm run build
+  npm install -g serve
+  serve -s build
 else
   cd server
   npm install
