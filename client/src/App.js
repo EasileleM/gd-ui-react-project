bca2d4ae67203f94c92ce1274cafb01afb367906
Aleksandr Kirilushkin <alexander.kirilushkin@gmail.com>
2019-10-29 20:17:53 +0400

    header finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
 import './components/header/header.scss';
+import Header from './components/header/header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
     </div>
   );
 }
--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -1,76 +1,67 @@
+import React from 'react';
+
 import './header.scss';
 
-const header = (
-  <header className='header'>
-    <section className="header__wrapper" >
-      <address className="header__social">
-        <span className="header__text header__text_md">
-          info@shopy.com
+class Header extends React.Component {
+  render() {
+    return (
+      <header className='header'>
+        <section className="header__wrapper header__wrapper_sm" >
+          <address className="header__social">
+            <span className="header__text header__text_md header__text_email">
+              info@shopy.com
             </span>
-        <span className="header__text header__text_md">
-          453 - 5553 - 996
+            <span className="header__text header__text_md header__text_phone">
+              453 - 5553 - 996
             </span>
-      </address>
-      <nav className="header__social">
-        <span className="header__icon">
-          facebook
-            </span>
-        <span className="header__icon">
-          twitter
-            </span>
-        <span className="header__icon">
-          google plus
-            </span>
-        <span className="header__icon">
-          instagram
-            </span>
-      </nav>
-    </section>
-    <section className="header__wrapper" >
-      <div className="header__logo">
-        <div>
-          sh
-              <span className="header__logo-image">
-          </span>
-          py
+          </address>
+          <nav className="header__social">
+            <div className="header__icon header__icon_facebook">
             </div>
-        <div>
-          shope any where
+            <div className="header__icon header__icon_twitter">
             </div>
-      </div>
-      <nav className="header__links">
-        <a className="header__text header__text_lg">
-          Home
+            <div className="header__icon header__icon_gplus">
+            </div>
+            <div className="header__icon header__icon_instagram">
+            </div>
+          </nav>
+        </section>
+        <section className="header__wrapper header__wrapper_lg">
+          <div className="header__logo">
+          </div>
+          <nav className="header__links-container">
+            <a className="header__text header__links-item header__text_lg header__links-item_active" href="https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md">
+              Home
             </a>
-        <a className="header__text header__text_lg">
-          Products
+            <a className="header__text header__links-item header__text_lg" href="https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md">
+              Products
             </a>
-        <a className="header__text header__text_lg">
-          Hot Deals
+            <a className="header__text header__links-item header__text_lg" href="https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md">
+              Hot Deals
             </a>
-        <a className="header__text header__text_lg">
-          About
+            <a className="header__text header__links-item header__text_lg" href="https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md">
+              About
             </a>
-        <a className="header__text header__text_lg">
-          Contact
+            <a className="header__text header__links-item header__text_lg" href="https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md">
+              Contact
             </a>
-        <form>
-          <input type="text" className="header__search-bar">
-            searchbar
+            <form className="header__search-container">
+              <input className="header__search-button" type="checkbox" id="search-button">
               </input>
-          <input type="button" className="header__icon">
-            search
+              <input className="header__search-bar" type="text">
               </input>
-        </form>
-        <a className="header__icon">
-          user
+              <label className="header__icon header__icon_search" for="search-button">
+              </label>
+            </form>
+            <a className="header__icon header__icon_big header__icon_user" href="https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md">
             </a>
-        <a className="header__icon">
-          basket
+            <a className="header__icon header__icon_big header__icon_cart" href="https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/anchor-is-valid.md">
             </a>
-      </nav>
-    </section>
-  </header>
-);
+          </nav>
+        </section>
+      </header>
+    );
+  }
+}
 
-export default header;
+export default Header;
