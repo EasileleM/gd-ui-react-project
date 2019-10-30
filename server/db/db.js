01702765eb20cc8adb565d41afc912ebad5cf762
fastndead <asmanumbetov@gmail.com>
2019-10-30 17:39:20 +0400

    closes #48

--- server/db/db.js
+++ server/db/db.js
@@ -2,28 +2,38 @@ import MongoDB from "mongodb";
 
 export class db {
     constructor() {
-        this.uri = process.env.MONGODB_URI  //"mongodb+srv://admin:qwerty123456789@react-vptyr.mongodb.net/test?retryWrites=true&w=majority";
+        this.uri = process.env.MONGODB_URI || "mongodb+srv://admin:qwerty123456789@react-vptyr.mongodb.net/test?retryWrites=true&w=majority";
         this.client = MongoDB.MongoClient(this.uri, {useNewUrlParser: true, useUnifiedTopology: true});
         this.dbName = "shop";
         this.dbConnection = this.client.connect();
     }
 
-    async getAll(collection) {
+    async getAll() {
         return this.dbConnection.then(client => {
             const db = client.db(this.dbName)
             return db
-                .collection(collection)
+                .collection("items")
                 .find({})
                 .toArray();
         });
     }
 
-    async getById(collection, id) {
+    async getById(id) {
         return this.dbConnection.then(client => {
             const db = client.db(this.dbName)
             return db
-                .collection(collection)
+                .collection("items")
                 .find({"id": id})
+                .toArray();
+        });
+    }
+
+    async getSlider(id) {
+        return this.dbConnection.then(client => {
+            const db = client.db(this.dbName)
+            return db
+                .collection("slider")
+                .find({"id": id })
                 .toArray();
         });
     }
