3c4924a748568105b9d8bdf7b6e85eacfa7db446
Umbetov Asman <33729448+fastndead@users.noreply.github.com>
2019-10-30 19:39:12 +0400

    closes #48 (#52)

--- server/pages/api/items.js
+++ server/pages/api/items.js
@@ -1,12 +1,21 @@
 import {db} from "../../db/db"
 
 const dbInstance = new db();
+const pageCapacity = 4;
+
 
 export default (req, res) => {
     dbInstance.getAll()
         .then(result => {
             res.statusCode = 200;
-            res.json(JSON.stringify(result));
+            if (req.query.page) {
+                const pageNumber = req.query.page;
+                const startIndex = (pageNumber - 1) * pageCapacity;
+                const endIndex = pageNumber * pageCapacity
+                res.json(JSON.stringify(result.slice(startIndex, endIndex)))
+            } else {
+                res.json(JSON.stringify(result));
+            }
         })
         .catch(err => {
             res.statusCode = 500;
