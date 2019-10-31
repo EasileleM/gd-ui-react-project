d356a3c8c138bf79d6750ed6feee2e89738d1516
Aleksandr Kirilushkin <alexander.kirilushkin@gmail.com>
2019-10-31 19:45:17 +0400

    App files cleared

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import './App.scss';
-import './components/header/header.scss';
-import Header from './components/header/index';
 
 function App() {
   return (
     <div className="App">
-      <Header />
     </div>
   );
 }
