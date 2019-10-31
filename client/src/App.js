3fad787e5a9f400b8c5d793dc09d29a367c7d775
Aleksandr Kirilushkin <alexander.kirilushkin@gmail.com>
2019-10-31 17:52:59 +0400

    App cleared

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import './App.scss';
-import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
-      <Footer />
     </div>
   );
 }
