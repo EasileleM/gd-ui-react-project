42c7b1ae8df5475bd655755333de8b05efea27f4
EasileleM <pro100prosavva@gmail.com>
2019-10-31 15:08:02 +0400

    improved rate logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import './App.scss';
 
-import {ItemCardSmall} from './components/index';
-
 function App() {
   return (
     <div className="App">
-      <ItemCardSmall />
     </div>
   );
 }
