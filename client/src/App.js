57cbc41ea24a36f63987c1fb9aff0f530b4d55fb
EasileleM <49760922+EasileleM@users.noreply.github.com>
2019-10-31 18:25:23 +0400

    Create Ads block (#49)
    * closed
    
    * closed again
    
    * feat: add sale-item
    
    * fix: fixed adaptation
    
    * fix: adaptation fixes and renamed sectionBlock
    
    * feat: add fetching

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import './App.scss';
-import {Slider} from './components/slider/index';
 
 function App() {
   return (
     <div className="App">
-      <Slider />
     </div>
   );
 }
