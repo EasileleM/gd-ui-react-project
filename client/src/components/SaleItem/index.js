4cb8b11beefd50d7762a1f25d0ee0c1cd441d7a1
EasileleM <pro100prosavva@gmail.com>
2019-10-31 18:06:02 +0400

    feat: add fetching

--- client/src/components/SaleItem/index.js
+++ client/src/components/SaleItem/index.js
@@ -5,27 +5,48 @@ import square1 from './assets/square-1.svg';
 import square2 from './assets/square-2.svg';
 
 export class SaleItem extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {};
+    this.loadResources();
+  }
+
+  async loadResources() {
+    const response = await fetch(`https://gd-ui-react-project-server.herokuapp.com/api/items/1`);
+
+    this.setState({
+      ready: true,
+      data: await response.json()
+    })
+  }
+
   render() {
-    return (
-      <div className="sale-item">
-        <div className="sale-item__sale">
-          <img src={square2} className="sale-item__decorator sale-item__decorator_2" alt="" />
-          <img src={square1} className="sale-item__decorator sale-item__decorator_1" alt="" />
-          <div className="sale-item__sale-text-block">
-            <p className="sale-item__sale-label">Sale</p>
-            <p className="sale-item__sale-size">50%</p>
+    if (this.state.ready) {
+      return (
+        <div className="sale-item">
+          <div className="sale-item__sale">
+            <img src={square2} className="sale-item__decorator sale-item__decorator_2" alt="" />
+            <img src={square1} className="sale-item__decorator sale-item__decorator_1" alt="" />
+            <div className="sale-item__sale-text-block">
+              <p className="sale-item__sale-label">Sale</p>
+              <p className="sale-item__sale-size">{this.state.data.sale + '%'}</p>
+            </div>
+          </div>
+          <div className="sale-item__item-info">
+            <h2 className="sale-item__item-name">{this.state.data.name}</h2>
+            <p className="sale-item__item-description">
+              {this.state.data.bundleInfo}
+            </p>
+          </div>
+          <div className="sale-item__purchase-info">
+            <button className="sale-item__bucket-button"></button>
+            <p className="sale-item__price">{this.state.data.price}<span className="sale-item__price_money-sign">$</span></p>
           </div>
         </div>
-        <div className="sale-item__item-info">
-          <h2 className="sale-item__item-name">Full winter kit</h2>
-          <p className="sale-item__item-description">
-            Half Jacket + Skiny Trousers + Boot leather
-        </p>
-        </div>
-        <div className="sale-item__purchase-info">
-          <button className="sale-item__bucket-button"></button>
-          <p className="sale-item__price">120<span className="sale-item__price_money-sign">$</span></p>
-        </div>
+      )
+    }
+    return (
+      <div className="sale-item">
       </div>
     )
   }
