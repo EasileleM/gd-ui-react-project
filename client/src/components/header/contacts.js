c958283c8e5e8cae0506160bcd834e3c641e4b30
Aleksandr Kirilushkin <alexander.kirilushkin@gmail.com>
2019-11-01 13:48:48 +0400

    header fix

--- client/src/components/header/contacts.js
+++ client/src/components/header/contacts.js
@@ -6,7 +6,7 @@ class Contacts extends React.Component {
   render() {
     return (
       <section className="header__wrapper header__wrapper_page header__wrapper_sm" >
-        <address className="header__wrapper">
+        <address className="header__wrapper header__wrapper_row">
           <span className="header__text header__text_md header__text_email">
             info@shopy.com
             </span>
@@ -14,7 +14,7 @@ class Contacts extends React.Component {
             453 - 5553 - 996
             </span>
         </address>
-        <nav className="header__wrapper">
+        <nav className="header__wrapper header__wrapper_row">
           <div className="header__icon header__icon_facebook" tabIndex="1">
           </div>
           <div className="header__icon header__icon_twitter" tabIndex="2">
