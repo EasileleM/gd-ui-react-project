42d2394e285b42714dc5c822beb90ae2d28ac983
EasileleM <pro100prosavva@gmail.com>
2019-11-03 15:11:27 +0400

    closed

---
+++ client/src/components/LoadingSpinner/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './main.scss';
+
+export function LoadingSpinner() {
+  return (
+    <div className="loading-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+  )
+}
