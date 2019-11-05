9e79a838732b5ca1c2da98ee1c8fd2ad5fe727d4
EasileleM <pro100prosavva@gmail.com>
2019-11-05 21:35:37 +0400

    fix: pagination endpoint in client

--- client/src/utils/loadCard.js
+++ client/src/utils/loadCard.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export async function loadCard(page = 1, size = 4) {
-  const result = await axios.get(`https://gd-ui-react-project-server.herokuapp.com/api/items?page=${page}&size=${size}`);
+  const result = await axios.get(`https://gd-ui-react-project-server.herokuapp.com/api/items/pagination?page=${page}&size=${size}`);
 
   this.setState({
     ready: true,
