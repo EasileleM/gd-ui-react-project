fd8a51c59b0b8cecc7b8fff040e180992edf6ac0
Mikhail <miffn3@yandex.ru>
2019-11-05 12:32:45 +0400

    feature: axios using for cards load

---
+++ client/src/utils/loadCard.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+export async function loadCard(page = 1, size = 4) {
+  const result = await axios.get(`https://gd-ui-react-project-server.herokuapp.com/api/items?page=${page}&size=${size}`);
+
+  this.setState({
+    ready: true,
+    cards: [...this.state.cards, ...result.data.items],
+    nextPage: result.data.nextPage
+  })
+}
