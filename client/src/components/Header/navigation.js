70312423e5429d9a596dbfbca35c8b1cd8dd4568
Aleksandr Kirilushkin <alexander.kirilushkin@gmail.com>
2019-11-07 19:27:01 +0400

    burger menu added

--- client/src/components/Header/navigation.js
+++ client/src/components/Header/navigation.js
@@ -15,23 +15,25 @@ class Navigation extends Component {
             <section className="header__wrapper header__wrapper_page header__wrapper_lg header__wrapper_column">
               <div className="header__logo">
               </div>
+              <input type="checkbox" id="headerMenuData" className="header__menu-data-input"></input>
               <nav className="header__links-container">
-                <a className="header__text header__links-item header__text_lg header__links-item_active" href="google.com">
+                <label onclick="" class="header__menu-button" htmlFor="headerMenuData" data-opened="⨯" data-closed="≡"></label>
+                <a className="header__burger-menu header__text header__links-item header__text_lg header__links-item_active" href="google.com">
                   {t('navigation.home')}
                 </a>
-                <a className="header__text header__links-item header__text_lg" href="google.com">
+                <a className="header__burger-menu header__text header__links-item header__text_lg" href="google.com">
                   {t('navigation.products')}
                 </a>
-                <a className="header__text header__links-item header__text_lg" href="google.com">
+                <a className="header__burger-menu header__text header__links-item header__text_lg" href="google.com">
                   {t('navigation.hotDeals')}
                 </a>
-                <a className="header__text header__links-item header__text_lg" href="google.com">
+                <a className="header__burger-menu header__text header__links-item header__text_lg" href="google.com">
                   {t('navigation.about')}
                 </a>
-                <a className="header__text header__links-item header__text_lg" href="google.com">
+                <a className="header__burger-menu header__text header__links-item header__text_lg" href="google.com">
                   {t('navigation.contact')}
                 </a>
-                <form className="header__search-container" method="POST" name="search">
+                <form className="header__burger-menu header__search-container" method="POST" name="search">
                   <input className="header__search-button" type="checkbox" id="search-button">
                   </input>
                   <input className="header__search-bar" type="text">
@@ -39,8 +41,8 @@ class Navigation extends Component {
                   <label className="header__icon header__icon_search" tabIndex="5" htmlFor="search-button">
                   </label>
                 </form>
-                <User />
-                <ShopCart />
+                <User/>
+                <ShopCart/>
               </nav>
             </section>
         }
