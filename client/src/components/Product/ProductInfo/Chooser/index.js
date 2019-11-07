380b585ce86728e724d319f3826bc4054fe7e936
Umbetov Asman <33729448+fastndead@users.noreply.github.com>
2019-11-07 20:47:54 +0400

    feature: related-products (#137)

--- client/src/components/Product/ProductInfo/Chooser/index.js
+++ client/src/components/Product/ProductInfo/Chooser/index.js
@@ -12,7 +12,7 @@ class Chooser extends Component {
                             <h2 className="chooser__heading_secondary">{t('productChooser.size')}:</h2>
                             <div className="chooser__sizes">
                                 {this.props.sizes.map(size => {
-                                    return <span onClick={() => {
+                                    return <span key={size} onClick={() => {
                                         this.props.handleSize(size)
                                     }} className={`chooser__size-value ${size === this.props.chosenSize ? "chooser__size-value_chosen" : ""}`}>
                                         {size}
@@ -22,9 +22,9 @@ class Chooser extends Component {
                         </div>
                         <div className="chooser__quantity">
                             <h2 className="chooser__heading_secondary">{t('productChooser.quantity')}:</h2>
-                            <div className="chooser__quantity-button" onClick={() => this.handleQuantity(true)}>+</div>
+                            <div className="chooser__quantity-button" onClick={() => this.props.handleQuantity(true)}>+</div>
                             <span className="chooser__quantity-number">{this.props.chosenQuantity}</span>
-                            <div className="chooser__quantity-button" onClick={() => this.handleQuantity(false)}>-</div>
+                            <div className="chooser__quantity-button" onClick={() => this.props.handleQuantity(false)}>-</div>
                         </div>
                     </div>
                 }
