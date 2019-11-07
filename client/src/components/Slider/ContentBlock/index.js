2ecfe9a038886b7856494f6390dce7f26e062e04
fastndead <asmanumbetov@gmail.com>
2019-11-07 18:05:56 +0400

    added slider links

--- client/src/components/Slider/ContentBlock/index.js
+++ client/src/components/Slider/ContentBlock/index.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 
 import { Item } from '../Item/index';
+import {Link} from "react-router-dom";
 
 export class ContentBlock extends PureComponent {
   constructor(props) {
@@ -12,7 +13,7 @@ export class ContentBlock extends PureComponent {
 
   render() {
     setTimeout(() => this.setState({ currentOffset: { top: (-this.props.switchTo * 650) } }));
-    return (
+      return (
       <div className="slider__content-block" style={this.state.currentOffset}>
         <Item data={this.props.items[0]} show={0 === Number(this.props.switchTo)} />
         <Item data={this.props.items[1]} show={1 === Number(this.props.switchTo)} />
