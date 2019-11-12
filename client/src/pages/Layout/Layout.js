44df14ee52bf6ac93408b64726c07057b7409151
fastndead <asmanumbetov@gmail.com>
2019-11-12 15:46:48 +0400

    conflict resolve

--- client/src/pages/Layout/Layout.js
+++ client/src/pages/Layout/Layout.js
@@ -21,6 +21,7 @@ import { CartWindow } from '../../components/CartWindow/CartWindow';
 import interceptor from '../../utils/interceptorResponse';
 import ScrollToTop from "../../components/SectionHeader/ScrollOnTop";
 import { changeBodyScrollState } from '../../utils/changeBodyScrollState';
+import Search from "../Search/Search";
 import fetchItems from '../../utils/cart/fetchItems';
 
 import store from '../../store';
@@ -50,6 +51,7 @@ class Layout extends Component {
                         <Switch>
                             <Route path="/" exact component={Home} />
                             <Route path="/item/:id" component={ProductDescriptionPage} />
+                            <Route path="/search" component={Search} />
                             <Route path="/404" component={NotFound} />
                             <Redirect to="/" />
                         </Switch>
