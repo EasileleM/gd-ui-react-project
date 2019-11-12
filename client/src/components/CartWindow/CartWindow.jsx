e2a458d27ee53654eb565bd93a082100d7967dc5
EasileleM <pro100prosavva@gmail.com>
2019-11-12 18:53:01 +0400

    restyled

--- client/src/components/CartWindow/CartWindow.jsx
+++ client/src/components/CartWindow/CartWindow.jsx
@@ -12,6 +12,7 @@ import './CartWindow.scss';
 export function CartWindow() {
   return (
     <div className="card-window">
+      <button className="card-window__background" onClick={() => store.dispatch(closeCart())}></button>
       <div className="card-window__content">
         <CloseButton onClick={() => store.dispatch(closeCart())}/>
         <Items />
