181346a3fa750ae9e767d5430fbbf1d4f6925163
Aleksandr Kirilushkin <44533511+KirilushkinAleksandr@users.noreply.github.com>
2019-11-12 19:25:30 +0400

    svg logo added (#173)

--- client/src/components/Header/cart.js
+++ client/src/components/Header/cart.js
@@ -5,7 +5,7 @@ import './main.scss';
 class ShopCart extends React.Component {
   render() {
     return (
-      <div className="header__icon-cart-wrapper">
+      <div className="header__icon-cart-wrapper header__icon_big">
         <p className="header__cart-counter">{this.props.cartSize}</p>
         <button onClick={() => this.props.handleOnClickOpenCart()} className="header__icon header__icon_big header__icon_cart">
         </button>
