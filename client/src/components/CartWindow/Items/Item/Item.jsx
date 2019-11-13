9894c85dc2ebd01215535b3a2b0dd7408d495be8
Mikhail <miffn3@yandex.ru>
2019-11-13 18:58:57 +0400

    feature: cart translation

--- client/src/components/CartWindow/Items/Item/Item.jsx
+++ client/src/components/CartWindow/Items/Item/Item.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Translation } from 'react-i18next';
 import store from '../../../../store';
 import removeItem from '../../../../utils/cart/removeItem';
 import updateItem from '../../../../utils/cart/updateItem';
@@ -20,39 +21,43 @@ export class Item extends React.Component {
 
   render() {
     return (
-      <div className="card-window__item cart-window-item">
-        <img src={this.props.data.generalData.images[0]} className="cart-window-item__image" alt="item" />
-        <div className="cart-window-item__info">
-          <h2 className="cart-window-item__name">
-            {this.props.data.generalData.name}
-          </h2>
-          <p className="cart-window-item__description">
-            {this.props.data.generalData.description}
-          </p>
-          <div className="cart-window-item__info-color-size">
-            <p className="cart-window-item__size-wrapper">
-              Size:
+      <Translation>
+        {t =>
+          <div className="card-window__item cart-window-item">
+            <img src={this.props.data.generalData.images[0]} className="cart-window-item__image" alt="item" />
+            <div className="cart-window-item__info">
+              <h2 className="cart-window-item__name">
+                {this.props.data.generalData.name}
+              </h2>
+              <p className="cart-window-item__description">
+                {this.props.data.generalData.description}
+              </p>
+              <div className="cart-window-item__info-color-size">
+                <p className="cart-window-item__size-wrapper">
+                  {t('orderItem.size')}:
               <span className="cart-window-item__size"> {this.props.data.size}</span>
-            </p>
-            <div className="cart-window-item__color-wrapper">
-              <p className="cart-window-item__color-text">Color: </p>
-              <div style={{ background: this.props.data.color }} className="cart-window-item__color"></div>
+                </p>
+                <div className="cart-window-item__color-wrapper">
+                  <p className="cart-window-item__color-text">{t('orderItem.color')}: </p>
+                  <div style={{ background: this.props.data.color }} className="cart-window-item__color"></div>
+                </div>
+                <p className="cart-window-item__price-wrapper">
+                {t('orderItem.price')}:
+              <span className="cart-window-item__price"> {this.props.data.generalData.price}{t('currency')}</span>
+                </p>
+              </div>
             </div>
-            <p className="cart-window-item__price-wrapper">
-              Price:
-              <span className="cart-window-item__price"> {this.props.data.generalData.price}$</span>
-            </p>
+            <div className="cart-window-item__item-controls">
+              <button onClick={() => store.dispatch(removeItem(store.getState(), this.props.data))} tabIndex="2" className="cart-window-item__item-controls-remove"></button>
+              <div className="cart-window-item__item-controls-amount-wrapper">
+                <button onClick={(event) => this.handleOnClickIncrement(event)} tabIndex="2" className="cart-window-item__item-controls-amount-button">+</button>
+                <input disabled tabIndex="2" maxLength="2" value={this.props.data.amount} className="cart-window-item__item-controls-amount-input" />
+                <button onClick={(event) => this.handleOnClickDecrement(event)} tabIndex="2" className="cart-window-item__item-controls-amount-button cart-window-item">-</button>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="cart-window-item__item-controls">
-          <button onClick={() => store.dispatch(removeItem(store.getState(), this.props.data))} tabIndex="2" className="cart-window-item__item-controls-remove"></button>
-          <div className="cart-window-item__item-controls-amount-wrapper">
-            <button onClick={(event) => this.handleOnClickIncrement(event)} tabIndex="2" className="cart-window-item__item-controls-amount-button">+</button>
-            <input disabled tabIndex="2" maxLength="2" value={this.props.data.amount} className="cart-window-item__item-controls-amount-input" />
-            <button onClick={(event) => this.handleOnClickDecrement(event)} tabIndex="2" className="cart-window-item__item-controls-amount-button cart-window-item">-</button>
-          </div>
-        </div>
-      </div>
+        }
+      </Translation>
     )
   }
 }
