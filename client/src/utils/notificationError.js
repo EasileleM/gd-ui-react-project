2bd1a4f1f3750bde094e351727c421e111b3ca04
Mikhail <miffn3@yandex.ru>
2019-11-14 14:44:29 +0400

    feature: notification favorites

--- client/src/utils/notificationError.js
+++ client/src/utils/notificationError.js
@@ -3,18 +3,22 @@ import { toast } from 'react-toastify';
 import i18n from '../i18n';
 
 export default function notificationError(messageRu, messageEn, error) {
-  let errorCode = error.message.match(/\d+/)[0];
-  if (errorCode >= 400 && errorCode < 500) {
-    if (i18n.language === 'ru') {
-      (() => toast(messageRu, { type: toast.TYPE.ERROR }))();
-    } else {
-      (() => toast(messageEn, { type: toast.TYPE.ERROR }))();
-    }
-  } else if (errorCode >= 500 && errorCode < 600) {
-    if (i18n.language === 'ru') {
-      (() => toast('Сервер недоступен, попробуйте вернуться позднее.', { type: toast.TYPE.ERROR }))();
-    } else {
-      (() => toast('Server is not available, try to reconnect later.', { type: toast.TYPE.ERROR }))();
+  const errCodes = error.message.match(/\d+/);
+  console.log(errCodes.size());
+  if (errCodes) {
+    const errorCode = errCodes[0];
+    if (errorCode >= 400 && errorCode < 500) {
+      if (i18n.language === 'ru') {
+        (() => toast(messageRu, { type: toast.TYPE.ERROR }))();
+      } else {
+        (() => toast(messageEn, { type: toast.TYPE.ERROR }))();
+      }
+    } else if (errorCode >= 500 && errorCode < 600) {
+      if (i18n.language === 'ru') {
+        (() => toast('Сервер недоступен, попробуйте вернуться позднее.', { type: toast.TYPE.ERROR }))();
+      } else {
+        (() => toast('Server is not available, try to reconnect later.', { type: toast.TYPE.ERROR }))();
+      }
     }
   }
 }
