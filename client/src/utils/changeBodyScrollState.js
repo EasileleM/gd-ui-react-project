5af3781cce483d795a0bd700317ee0099c4a1b9e
EasileleM <pro100prosavva@gmail.com>
2019-11-15 14:11:30 +0400

    hotfix

--- client/src/utils/changeBodyScrollState.js
+++ client/src/utils/changeBodyScrollState.js
@@ -1,10 +1,2 @@
 export function changeBodyScrollState(state) {
-  const body = document.getElementsByTagName('body')[0];
-  if (state) {
-    body.classList.add('scroll-disabled');
-    body.classList.remove('scroll-enabled');
-  } else {
-    body.classList.add('scroll-enabled');
-    body.classList.remove('scroll-disabled');
-  }
 }
