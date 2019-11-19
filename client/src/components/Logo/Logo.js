9ee02130e2f69a77c1705d4639f77fdfba23b670
Aleksandr Kirilushkin <44533511+KirilushkinAleksandr@users.noreply.github.com>
2019-11-19 12:14:30 +0400

    tests for logo (#225)

--- client/src/components/Logo/Logo.js
+++ client/src/components/Logo/Logo.js
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 import './main.scss';
 
 export class Logo extends React.Component {
+  componentDidMount() {}
+
   render() {
     return (
       <Link className="logo" to="/">
