0c1794ddcbdd5ab64c770b3fc07d8519d39d62f0
EasileleM <pro100prosavva@gmail.com>
2019-11-19 18:46:30 +0400

    section header tests

---
+++ client/src/components/SectionHeader/__tests__/index.spec.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {mount, shallow} from 'enzyme';
+import {expect} from 'chai';
+import sinon from 'sinon';
+import {configure} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { SectionHeader } from "../index";
+
+configure({adapter: new Adapter()});
+
+describe('<SectionHeader />', () => {
+  let sandbox;
+  beforeEach(function () {
+    sandbox = sinon.createSandbox();
+  });
+
+  afterEach(function () {
+    sandbox.restore();
+  });
+
+  it('calls componentDidMount', () => {
+    sandbox.spy(SectionHeader.prototype, 'componentDidMount');
+    const wrapper = mount(<SectionHeader t={key => key}/>);
+    expect(SectionHeader.prototype.componentDidMount).to.have.property('callCount', 1);
+  });
+
+}); 
--- client/src/components/SectionHeader/index.js
+++ client/src/components/SectionHeader/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 
 import './main.scss';
 
-export const SectionHeader = ({ title_colored, title, description }) => {
-  return (
-    <div className='section-header'>
-      <div className='section-header__title'>
-        <span className='section-header__title section-header__title_colored'>{title_colored}</span>&nbsp;{title}
+export class SectionHeader extends React.Component {
+  componentDidMount() {}
+  render() {
+    return (
+      <div className='section-header'>
+        <div className='section-header__title'>
+          <span className='section-header__title section-header__title_colored'>{this.props.title_colored}</span>&nbsp;{this.props.title}
+        </div>
+        <div className='section-header__description'>
+          {this.props.description}
+        </div>
       </div>
-      <div className='section-header__description'>
-        {description}
-      </div>
-    </div>
-  );
+    );
+  }
 }
