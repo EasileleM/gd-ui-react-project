fbc91e27fe81f3381cd300843d8363808e392480
fastndead <asmanumbetov@gmail.com>
2019-12-02 20:24:39 +0400

    fix constant

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,4 +1,4 @@
-export const SERVER_URL = "https://gd-ui-react-project-client.herokuapp.com";
+export const SERVER_URL = "https://gd-ui-react-project-server.herokuapp.com";
 export const SLIDER_HEIGHT = 650;
 export const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 export const PASSWORD_REGEX = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
