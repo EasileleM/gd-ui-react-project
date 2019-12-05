b16ba8760ba2416f34e10abde9acc6e5e263a9ba
EasileleM <pro100prosavva@gmail.com>
2019-12-05 14:40:50 +0400

    removed redundant comments

--- client/src/utils/getAuthentificationInfo.js
+++ client/src/utils/getAuthentificationInfo.js
@@ -8,22 +8,4 @@ export function getAuthentificationInfo() {
   .then((res) => {
     return res.data;
   });
-  // const mockRawData = { "_id": "5db895dc5ebc0ec021d6d683", "name": "Abibas Kid Boots", "bundleInfo": "Casual kit", "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry", "price": "750", "sizes": ["S", "M", "L", "XL"], "colors": ["#4287f5", "#f54242", "#400a2d"], "images": [{ "src": "https://cdn-images.farfetch-contents.com/14/18/04/06/14180406_21273275_1000.jpg", "srcset": ["https://cdn-images.farfetch-contents.com/14/18/04/06/14180406_21273275_1000.jpg 1000w", "https://cdn-images.farfetch-contents.com/14/18/04/06/14180406_21273275_500.jpg 500w", "https://cdn-images.farfetch-contents.com/14/18/04/06/14180406_21273275_800.jpg 800w", "https://cdn-images.farfetch-contents.com/14/18/04/06/14180406_21273275_300.jpg 300w"] }], "sale": "45", "rating": "3", "categories": ["Shoes", "kids"], "brand": "Abibas", "creationDate": "2018-10-21T20:00:00.000Z" };
-
-  // return Promise.resolve({
-  //   user: {
-  //     info: {
-  //       firstName: 'yes',
-  //       lastName: 'Mb'
-  //     },
-  //     cartItems: [
-  //       {
-  //         generalData: mockRawData,
-  //         color: "#4287f5",
-  //         amount: 2,
-  //         size: "XL"
-  //       }
-  //     ]
-  //   }
-  // });
 }
