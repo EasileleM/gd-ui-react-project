11c732cb2a5fe957abe819fe17803245c95f5040
EasileleM <pro100prosavva@gmail.com>
2019-12-09 18:52:21 +0400

    fixed jumping checkboxes in product card

---
+++ client/src/components/ProductCard/ColorSelector/ColorSelector.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import './ColorSelector.scss';
+
+export class ColorSelector extends React.Component {
+
+  render() {
+    const colorVariants = this.props.colors.map((color) =>
+      <div className="color-selector-container-wrapper">
+        <label key={color}
+          style={{
+            backgroundColor: color
+          }}
+          className={`color-selector__container ${this.props.selectedOption === color ? 'color-selector__container_checked' : ''}`}>
+
+          <input type="radio"
+            name="color"
+            value={color}
+            checked={this.props.selectedOption === color}
+            onChange={(e) => this.props.handleOptionChange(e)}
+            className="color-selector__button"
+          />
+        </label>
+      </div>
+
+    )
+    return (
+      <div className="color-selector">
+        <form className="color-selector__form">
+          {colorVariants}
+        </form>
+      </div>
+    );
+  }
+}
---
+++ client/src/components/ProductCard/SizesSelector/SizesSelector.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { withTranslation } from 'react-i18next';
+
+import './SizesSelector.scss';
+
+export class SizesSelector extends React.Component {
+  render() {
+    const t = this.props.t;
+    const dash = <span className="sizes-selector__dash">-</span>;
+    const sizeVariants = this.props.sizes.map((size, index) =>
+      <>
+        <div key={size} className="sizes-selector__size">
+          <label key={index} className={`sizes-selector__label ${this.props.selectedOption === size ? 'sizes-selector__label_checked' : ''}`}>
+            {size}
+            <input type="radio"
+              onChange={(e) => this.props.handleOptionChange(e)}
+              name="size"
+              value={size}
+              checked={this.props.selectedOption === size}
+              className="sizes-selector__input" />
+          </label>
+        </div>
+        {this.props.sizes.length - 1 === index ? null : dash}
+      </>
+    )
+    return (
+      <div className="sizes-selector">
+        <div className="sizes-selector__title">
+          {t('productCard.size')}:&nbsp;
+        </div>
+        <form className="sizes-selector__form">
+          {sizeVariants}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default withTranslation()(SizesSelector);
