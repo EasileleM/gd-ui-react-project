5da5fae82b5819f5ec3f08048f3f4f0557bb5ed1
EasileleM <pro100prosavva@gmail.com>
2019-12-19 21:56:54 +0400

    about page

--- client/src/pages/AboutPage/AboutPage.jsx
+++ client/src/pages/AboutPage/AboutPage.jsx
@@ -4,18 +4,25 @@ import { withTranslation } from 'react-i18next';
 import './AboutPage.scss';
 
 import { SectionHeader } from '../../components/SectionHeader/SectionHeader.jsx';
+import { PictureWithHeader } from '../../components/PictureWithHeader/PictureWithHeader.jsx';
+
+import aboutImageHeader from '../../assets/aboutImageHeader.jpeg';
+import PersonBlock from '../../components/PersonBlock/PersonBlock';
 
 export class AboutPage extends React.Component {
   render() {
-    return <div className="contact">
-      <SectionHeader
-        additionalClass="section-header_margin-small"
-        title_colored={this.props.t('about.our-team-left')}
-        title={this.props.t('about.our-team-right')} />
-        <SectionHeader
-        additionalClass="section-header_margin-small"
-        title_colored={this.props.t('about.our-mentors-left')}
-        title={this.props.t('about.our-mentors-right')} />
+    return <div className="about">
+      <PictureWithHeader headerLeft={this.props.t('about.team.header.left')} headerRight={this.props.t('about.team.header.right')} image={aboutImageHeader} />
+      <PersonBlock />
+      <SectionHeader title_colored={this.props.t('about.shopInfo.left')} title={this.props.t('about.shopInfo.right')} />
+      <div className="about__info-wrapper">
+        <p className="about__info">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis voluptates expedita voluptate odit dicta, explicabo, nemo non in itaque eius ipsam necessitatibus commodi praesentium quisquam quae distinctio error labore illo.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis voluptates expedita voluptate odit dicta, explicabo, nemo non in itaque eius ipsam necessitatibus commodi praesentium quisquam quae distinctio error labore illo.
+      </p>
+        <p className="about__info">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis voluptates expedita voluptate odit dicta.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis voluptates expedita voluptate odit dicta.
+      </p>
+      </div>
     </div>;
   }
 }
