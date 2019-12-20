3a119ac190ad251ba9eb61cbc0f5cee7a02140ad
Umbetov Asman <33729448+fastndead@users.noreply.github.com>
2019-12-20 11:57:05 +0400

    fix/hot-deals-translation (#346)

--- client/src/utils/loadHotDealsThisMonth.js
+++ client/src/utils/loadHotDealsThisMonth.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import {SERVER_URL} from '../constants/constants';
+import i18n from '../i18n';
+
 
 export default function loadHotDealsThisMonth(page = 1, size = 8) {
-  let url = `${SERVER_URL}/api/items/hot-deals-this-month?size=${size}`;
+  let url = `${SERVER_URL}/api/items/hot-deals-this-month?size=${size}&lang=${i18n.language}`;
   return axios.get(url);
 }
